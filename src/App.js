0916fdda5d29ab6533ac723871c87ad6d7b12639
Kiran Babu <kiranbabu7675@gmail.com>
2022-01-11 01:44:30 +0530

    style: Completed Nav bar and hero section is in progress

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ function Nav() {
   return (
     <header class="main-head">
       <nav>
-        <h1 class="logo">Augmetriz .tech</h1>
+        <h1 class="logo">
+          Augmetriz <span>.tech</span>
+        </h1>
         <ul class="nav-links">
           <li>
-            <a href="#home">Home</a>
+            <a href="#home" className="active">
+              Home
+            </a>
           </li>
           <li>
             <a href="#about">About</a>
@@ -21,4 +25,35 @@ function Nav() {
   );
 }
 
-export default Nav;
+function Home() {
+  return (
+    <section className="hero">
+      <div className="intro-text">
+        <h1>Teenathon</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+        <div className="intro-btns">
+          <button>Know More</button>
+          <button>Sign Up</button>
+        </div>
+      </div>
+      <div className="intro-img">
+        <img src="/images/cyborg-sign-up.png" />
+      </div>
+    </section>
+  );
+}
+function App() {
+  return (
+    <>
+      <Nav></Nav>
+      <Home></Home>
+    </>
+  );
+}
+
+export default App;

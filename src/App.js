442cf1037b3249f255c783662034f607668eb8be
Kiran Babu <kiranbabu7675@gmail.com>
2022-01-10 23:54:16 +0530

    style: Nav bar is in progress

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+function Nav() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header class="main-head">
+      <nav>
+        <h1 class="logo">Augmetriz .tech</h1>
+        <ul class="nav-links">
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#hackathon">Hackathon</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
 
-export default App;
+export default Nav;
